6905e5080a524f92d4da6bbe3c0292b4d1a71b91
Santiago02 <santimedinaa2016@gmail.com>
2022-08-31 21:32:19 -0300

    Limpieza + API de clima

--- indice.js/index.js
+++ indice.js/index.js
@@ -1,4 +1,4 @@
-    const baseDeDatos = [
+const baseDeDatos = [
     {
         id: 1,
         nombre: 'Lienzo',
@@ -26,6 +26,20 @@
         ancho: '1.40',
         precio: 1900,
         imagen: 'pages1/blackout.jpg'
+    },
+    {
+        id: 5,
+        nombre: 'Batista',
+        ancho: '1.50',
+        precio: 550,
+        imagen: 'pages1/batista.jpg'
+    },
+    {
+        id: 6,
+        nombre: 'Microfibra',
+        ancho: '1.50',
+        precio: 750,
+        imagen: 'pages1/microfibra.jpg'
     }
 
 ];
@@ -174,9 +188,56 @@ cargarCarritoDeLocalStorage();
 renderizarProductos();
 renderizarCarrito();
 
-Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
-})
+
+document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+let theme = "dark";
+    const root = document.querySelector(":root");
+    const container = document.getElementsByClassName("theme-container")[0];
+    const themeIcon = document.getElementById("theme-icon");
+    container.addEventListener("click", setTheme);
+    function setTheme() {
+    switch (theme) {
+        case "dark":
+        setLight();
+        theme = "light";
+        break;
+        case "light":
+        setDark();
+        theme = "dark";
+        break;
+    }
+
+    }
+    function setLight() {
+    root.style.setProperty(
+        "--bs-dark",
+        "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+    );
+    container.classList.remove("shadow-dark");
+    setTimeout(() => {
+        container.classList.add("shadow-light");
+        themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = sun;
+    }
+    function setDark() {
+    root.style.setProperty("--bs-dark", "#212529");
+    container.classList.remove("shadow-light");
+    setTimeout(() => {
+        container.classList.add("shadow-dark");
+        themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = moon;
+    };
+
+Toastify({
+        text: "This is a toast",
+        
+        duration: 3000
+        
+        }).showToast();
